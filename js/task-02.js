37d79b295960af6e3f240beb5e84b96b91e4feb3
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-23 14:57:24 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+const itemsArray = [];
+
+for (const ing of ingredients) {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ing;
+  itemEl.classList.add("item");
+  itemsArray.push(itemEl);
+}
+ingredientsListEl.append(...itemsArray);
