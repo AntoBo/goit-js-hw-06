f884135d57d780307593b5de377805b4d651e0b2
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-28 19:49:54 +0200

    task 5 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,12 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('input', () => {
-  //   console.log('input detected!');
-  console.log(inputEl.textContent);
+function onInput(event) {
+  if (event.currentTarget.value) {
+    outputEl.textContent = event.currentTarget.value;
+  } else {
+    outputEl.textContent = 'Anonymous';
+  }
+}
 
-  outputEl.textContent = inputEl.textContent;
-});
-
-console.log(outputEl.textContent);
-console.log(inputEl.textContent);
+inputEl.addEventListener('input', onInput);
