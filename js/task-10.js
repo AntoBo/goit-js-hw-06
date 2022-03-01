396ed873a665c88958d77ce45d018fd272f01c4c
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-01 21:46:09 +0200

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,29 @@
-const numberEl = document.querySelector('#controls input[type="number"]');
+const boxesEl = document.querySelector('#boxes');
+
+const createBtn = document.querySelector('button[data-create]');
+createBtn.addEventListener('click', createBoxes);
+
+const destroyBtn = document.querySelector('button[data-destroy]');
+destroyBtn.addEventListener('click', destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  amount = document.querySelector('input[type="number"]').value;
+  let injectHTML = '';
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    injectHTML += `<div style='width:${boxSize}px; height:${boxSize}px; margin:10px; background-color:${getRandomHexColor()}'></div>`;
+    boxSize += 10;
+  }
+  boxesEl.insertAdjacentHTML('beforeend', injectHTML);
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
 }
