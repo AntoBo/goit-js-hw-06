6cc8e5f61dd7ac31dc1801bd33cf19948bebb814
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-01 15:04:08 +0200

    task 8 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,9 @@
 const textEl = document.querySelector('#text');
 const rangeControlEl = document.querySelector('#font-size-control');
 
+rangeControlEl.value = 18;
+console.log(textEl.style.fontSize);
+
 function onRangeChange(event) {
   textEl.style.fontSize = `${event.currentTarget.value}px`;
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+// alert('Hello');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const values = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+
+  if (values.email && values.password) {
+    event.currentTarget.reset();
+    return console.log(values);
+  } else alert('Заповніть усі поля!');
+}
