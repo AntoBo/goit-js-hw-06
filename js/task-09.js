85cd7b700bee89f81adb8db611e42d92dd895485
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-01 15:28:33 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyEl = document.querySelector('body');
+const colorSpanEl = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+btnChangeColor.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  const newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newColor;
+  colorSpanEl.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
